90855d580422aa0435b3d79191fda1d2327415e5
iceiceice <84047808+IndianBleach@users.noreply.github.com>
2024-12-15 21:26:45 +0300

    stack insertion-sort

--- stacks/basic_stack.py
+++ stacks/basic_stack.py
@@ -8,6 +8,8 @@ class basic_stack:
         return self.__cursor
     
     def top(self):
+        if (self.__cursor == 0):
+            return None
         return self.__data[self.__cursor-1]
 
     def pop(self):
@@ -30,18 +32,52 @@ class basic_stack:
             cur = self.pop()
 
         return st
+    
+    def as_list(self):
+        ls = []
+        while(self.top() != None):
+            ls.append(self.pop())
+        
+        rev = reversed(ls)
+        for el in rev:
+            self.push(el)
+
+        return ls
+
+    def sort(self):
+        q = basic_stack()
+        r = basic_stack()
+        count = self.size()
+        while(count > 0):
+            # 5 (4)
+            el = self.pop()
+            if (r.top() != None and r.top() < el):
+                r.push(el)
+            else:
+                # 3 4 (2)
+                while(r.top() != None and
+                      r.top() >= el):
+                    q.push(r.pop())
+
+                r.push(el) 
+
+                while(q.top() != None):
+                    r.push(q.pop())
+            
+            count -= 1
+        
+        return r
+
+
 
 t = basic_stack()
 t.push(1)
-t.push(2)
-t.push(3)
+t.push(6)
 t.push(4)
+t.push(3)
+t.push(7)
+t.push(2)
 
 
-rev  = t.reverse()
-print(rev.size())
-print(rev.pop())
-print(rev.pop())
-print(rev.pop())
-print(rev.pop())
-print(rev.pop())
+st = t.sort()
+print(st.as_list())
